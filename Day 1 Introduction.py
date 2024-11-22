aea4da115d96977846c237cdef8523efaa3b4007
santiquinterog <santiago990101@hotmail.com>
2024-11-22 17:44:02 -0500

    fix: remove code

--- Day 1 Introduction.py
+++ Day 1 Introduction.py
@@ -1,5 +1,3 @@
-import math
-
 # Imprimir Hello, World!
 print("Hello, world!")
 
@@ -8,7 +6,7 @@ Comentarios en varias lineas
 '''
 
 """
-Ver tipo de dato
+Tipos
 """
 
 print(type(10)) # Int
@@ -20,21 +18,3 @@ print(type([1,2,3])) # List
 print(type({"name": "Python"})) # Dictionary
 print(type((1,2,3))) # Tuple
 print(type({1,2,3})) # Set
-
-
-# Algunas operaciones
-print(2 + 3)  # addition(+)
-print(3 - 1)  # subtraction(-)
-print(2 * 3)  # multiplication(*)
-print(3 / 2)  # division(/)
-print(3 // 2) # Floor division operator(//) División redondeada hacia abajo
-print(3 ** 2) # exponential(**)
-print(3 % 2)  # modulus(%)
-
-""" Distancia Euclidiana """
-p1 = (2, 3)
-p2 = (10, 8)
-
-# Fórmula para la distancia euclidiana
-distance = math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
-print("Distancia euclidiana:", distance)
